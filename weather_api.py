27c18a5c2395f850207134d6951c47a803dbc7c5
eka Ponkratova <kate.ponkratova@gmail.com>
2023-12-17 12:43:06 +0800

    updated existing scripts

--- weather_api.py
+++ weather_api.py
@@ -8,11 +8,11 @@ import duckdb
 from pandas.io.json import json_normalize
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename="logs.log",level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 #passing weather API params
 BASE_URL = "http://api.weatherapi.com/v1/current.json"
-API_KEY = "212a0353103949f68ff83745231112"
+API_KEY ="212a0353103949f68ff83745231112"
 q = "London"
 
 #creating a database connection and table
@@ -36,17 +36,23 @@ conn.execute(sql)
 
 def call_api(BASE_URL, API_KEY, q):
     """The functions calls a weather API to extract the current forecast"""
-    resp = requests.get(f"{BASE_URL}?key={API_KEY}&q={q}")
-    json_response = resp.json()       
-    objects = json_normalize(json_response)
-    #extracting only required columns
-    objects = objects[["location.name", "location.region", "location.lat", "location.lon", 'current.precip_in', "current.humidity",
-              "current.cloud", "current.feelslike_c", "current.feelslike_f",  "current.vis_km",  "current.vis_miles" , "current.uv",
-              "current.gust_mph", "current.gust_kph"  ]]
-    #renaming column names
-    objects.columns = ["name", "region", "lat", "lon", "precip_in", "humidity", "cloud", "feelslike_c", "feelslike_f",
-                       "vis_km", "vis_miles", "uv", "gust_mph", "gust_kph" ]
-    return objects
+    try:
+        resp = requests.get(f"{BASE_URL}?key={API_KEY}&q={q}")
+        json_response = resp.json()       
+        objects = json_normalize(json_response)
+        #extracting only required columns
+        objects = objects[["location.name", "location.region", "location.lat", "location.lon", 'current.precip_in', "current.humidity",
+                "current.cloud", "current.feelslike_c", "current.feelslike_f",  "current.vis_km",  "current.vis_miles" , "current.uv",
+                "current.gust_mph", "current.gust_kph"  ]]
+        #renaming column names
+        objects.columns = ["name", "region", "lat", "lon", "precip_in", "humidity", "cloud", "feelslike_c", "feelslike_f",
+                        "vis_km", "vis_miles", "uv", "gust_mph", "gust_kph" ]
+        resp.raise_for_status()
+        logging.info('API connectivity check passed.')
+        return objects
+    except requests.exceptions.RequestException as e:
+        logging.error(f'API connectivity check failed: {e}')
+        return False
 
 def save_data_to_db():
     """The function saves the output file as a duckdb"""
@@ -57,8 +63,10 @@ def save_data_to_db():
     except Exception as e:
         logging.error(f'Failed to save data to duckdb: {e}')
 
-save_data_to_db()
+# Main execution
+if __name__ == '__main__':
+    save_data_to_db()
 
 #testing the output
-conn.execute("SELECT * from curr_weather ").fetchdf()
+#conn.execute("SELECT * from curr_weather ").fetchdf()
 
